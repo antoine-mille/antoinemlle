3c1e434e4339e411b29c404044f0165a4d5f740a
Antoine Mille <antoine.mille3@gmail.com>
2024-10-01 13:50:35 +0200

    feat: create services page translations

--- components/review-section.tsx
+++ components/review-section.tsx
@@ -1,12 +1,17 @@
 import { ReviewMarquee } from "@/components/review-marquee"
 import { Section } from "@/components/section"
+import { useTranslations } from "next-intl"
 
 const ReviewSection = () => {
+  const t = useTranslations("ReviewSection")
   return (
     <Section className="space-y-6">
       <h2 className="text-xl text-gray-900 sm:text-2xl">
-        Retrouvez des <span className="font-semibold text-secondary">avis</span>{" "}
-        sur <span className="font-semibold text-secondary">mes services</span>
+        {t.rich("title", {
+          span: (chunks) => (
+            <span className="font-semibold text-secondary">{chunks}</span>
+          ),
+        })}
       </h2>
       <ReviewMarquee />
     </Section>
--- components/services-section.tsx
+++ components/services-section.tsx
@@ -3,34 +3,41 @@ import { Section } from "@/components/section"
 
 import designImage from "@/public/images/ui-ux-design.svg"
 import webDevelopmentImage from "@/public/images/web-development.svg"
+import { getTranslations } from "next-intl/server"
 
-const services = [
-  {
-    title: "Développement Web",
-    description:
-      "Je développe des sites web modernes, réactifs et performants.",
-    imageUrl: webDevelopmentImage,
-  },
-  {
-    title: "Design UI/UX",
-    description:
-      "Je crée des interfaces utilisateur sur mesure pour vos applications.",
-    imageUrl: designImage,
-  },
-]
+async function getServices() {
+  const t = await getTranslations({ namespace: "ServiceCard" })
+  return [
+    {
+      title: t("WebDevelopment.title"),
+      description: t("WebDevelopment.description"),
+      imageUrl: webDevelopmentImage,
+    },
+    {
+      title: t("Design.title"),
+      description: t("Design.description"),
+      imageUrl: designImage,
+    },
+  ]
+}
 
-const ServicesSection = () => {
+const ServicesSection = async () => {
+  const t = await getTranslations({ namespace: "ServiceSection" })
+  const services = await getServices()
+
   return (
     <Section withDotPattern className="flex flex-col gap-6">
       <h1 className="text-center text-2xl font-semibold text-gray-900 sm:text-3xl lg:text-4xl">
-        Découvrez les services <br /> que je propose !
+        {t.rich("title", {
+          br: () => <br />,
+        })}
       </h1>
       <div className="grid gap-8 sm:grid-cols-2">
         {services.map((service) => (
           <div key={service.title} className="flex flex-col items-center gap-2">
             <Image
               src={service.imageUrl}
-              alt={service.title}
+              alt={t("imageAlt", { service: service.title })}
               width={208}
               height={208}
               className="size-52 object-cover"
