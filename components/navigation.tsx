08bf5be11d617a794e4291a0dd18c3a97b0b5154
Antoine Mille <antoine.mille3@gmail.com>
2024-10-02 17:24:35 +0200

    feat: add blog link

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -11,6 +11,8 @@ import { LinkedinIcon, MenuIcon } from "lucide-react"
 import { LinkIcon } from "@/components/link-icon"
 import { getTranslations } from "next-intl/server"
 import { LanguageSelect } from "@/components/language-select"
+import { cn } from "@/lib/utils"
+import { Badge } from "./badge"
 
 async function getLinks() {
   const t = await getTranslations({ namespace: "Links" })
@@ -19,6 +21,7 @@ async function getLinks() {
     { href: "/#projects", label: t("portfolio") },
     { href: "/career", label: t("career") },
     { href: "/#contact", label: t("contact") },
+    { href: "/blog", label: t("blog"), additionalText: "Soon" },
   ]
 }
 
@@ -35,11 +38,8 @@ const HeaderLeft = () => {
 
 const MobileNavigation = async () => {
   const links = await getLinks()
-  const mobileNavigation = await getTranslations({
+  const t = await getTranslations({
     namespace: "MobileNavigation",
-  })
-  const languageSwitcher = await getTranslations({
-    namespace: "LanguageSwitcher",
   })
 
   return (
@@ -48,24 +48,30 @@ const MobileNavigation = async () => {
         <MenuIcon className="size-6" />
       </SheetTrigger>
       <SheetContent className="flex flex-col gap-0">
-        <SheetTitle className="text-gray-900">
-          {mobileNavigation("title")}
-        </SheetTitle>
-        <SheetDescription>{mobileNavigation("description")}</SheetDescription>
+        <SheetTitle className="text-gray-900">{t("title")}</SheetTitle>
+        <SheetDescription>{t("description")}</SheetDescription>
         <nav className="mt-20 flex flex-col items-end gap-4">
           {links.map((link) => (
             <Link
               key={link.href}
               href={link.href}
-              className="text-lg font-semibold uppercase text-primary transition-colors duration-300 hover:text-secondary"
+              className={cn(
+                "text-lg font-semibold uppercase text-primary transition-colors duration-300 hover:text-secondary",
+                link.additionalText && "relative mt-2"
+              )}
             >
               <SheetClose>{link.label}</SheetClose>
+              {link.additionalText && (
+                <Badge className="absolute left-0 -translate-x-full -translate-y-2/3 border border-secondary bg-white text-xs text-secondary transition-colors duration-300 hover:bg-secondary/10">
+                  {link.additionalText}
+                </Badge>
+              )}
             </Link>
           ))}
         </nav>
         <div className="mt-auto space-y-3 border-t pt-5">
           <p className="inline-flex items-center gap-3 text-sm text-gray-900">
-            {mobileNavigation("socials")}
+            {t("socials")}
             <LinkIcon href="https://www.linkedin.com/in/antoinemlle/">
               <LinkedinIcon className="size-5 lg:size-6" />
             </LinkIcon>
@@ -80,15 +86,23 @@ const MobileNavigation = async () => {
 const Navigation = async () => {
   const links = await getLinks()
   return (
-    <nav className="hidden gap-12 sm:flex">
+    <nav className="hidden gap-14 sm:flex">
       <div className="flex items-center gap-4 md:gap-6 lg:gap-8">
         {links.map((link) => (
           <Link
             key={link.href}
             href={link.href}
-            className="font-semibold text-gray-900 transition-colors duration-300 hover:text-secondary"
+            className={cn(
+              "font-semibold text-gray-900 transition-colors duration-300 hover:text-secondary",
+              link.additionalText && "relative"
+            )}
           >
             {link.label}
+            {link.additionalText && (
+              <Badge className="absolute -translate-y-2/3 border border-secondary bg-white text-xs text-secondary transition-colors duration-300 hover:bg-secondary/10">
+                {link.additionalText}
+              </Badge>
+            )}
           </Link>
         ))}
       </div>
