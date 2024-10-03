2f6ae19b77e6bbf013a42ec9c6f728d4b41af23c
Antoine Mille <antoine.mille3@gmail.com>
2024-10-03 11:10:30 +0200

    fix: middleware matchers for docs

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,5 @@ export default createMiddleware(routing)
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|docs).*)"],
 }
